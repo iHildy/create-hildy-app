43c6c2e45d17133d1d5844cc2f2522a9b6a960a0
iHildy <ian@jedi.net>
2025-07-17 14:23:27 -0500

    added eslintPluginPrettierRecommended

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import pluginQuery from '@tanstack/eslint-plugin-query';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginQuery from "@tanstack/eslint-plugin-query";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,9 +12,17 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  { ignores: ['next.config.js', 'eslint.config.js', '.lintstagedrc.js', '.vercel'] },
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-  ...pluginQuery.configs['flat/recommended'],
+  {
+    ignores: [
+      "next.config.js",
+      "eslint.config.js",
+      ".lintstagedrc.js",
+      ".vercel",
+    ],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...pluginQuery.configs["flat/recommended"],
+  eslintPluginPrettierRecommended,
 ];
 
 export default eslintConfig;
